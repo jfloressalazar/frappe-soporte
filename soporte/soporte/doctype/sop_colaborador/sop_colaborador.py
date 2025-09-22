655794b7080303cc62a2cddb00aee5c2b43510f8
Jesus Flores <jfloresssalazar91@gmail.com>
2025-09-22 13:06:13 -0600

    Avance con desarrollo y actualización de fixtures

--- soporte/soporte/doctype/sop_colaborador/sop_colaborador.py
+++ soporte/soporte/doctype/sop_colaborador/sop_colaborador.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2025, jesus flores and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SOPCOLABORADOR(Document):
-	pass
+
+	def before_insert(self):
+		self.nombre_colaborador=" ".join([self.nombres_colaborador, self.apellidos_colaborador])
+
+	def before_save(self):
+		self.nombre_colaborador=" ".join([self.nombres_colaborador, self.apellidos_colaborador])
---
+++ soporte/soporte/doctype/sop_marcaequipo/test_sop_marcaequipo.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, jesus flores and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSOPMARCA(FrappeTestCase):
+	pass
