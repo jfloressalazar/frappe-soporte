6a01516d0c417c7040c6814ece91575b6f038b68
Jesus Flores <jfloresssalazar91@gmail.com>
2025-09-23 13:22:06 -0600

    Avance con filtros y datos para asignaciones

--- soporte/soporte/doctype/sop_asignacionequipo/sop_asignacionequipo.js
+++ soporte/soporte/doctype/sop_asignacionequipo/sop_asignacionequipo.js
@@ -1,8 +1,25 @@
 // Copyright (c) 2025, jesus flores and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("SOP-ASIGNACIONEQUIPO", {
-// 	refresh(frm) {
+frappe.ui.form.on("SOP-ASIGNACIONEQUIPO", {
+    refresh: function (frm) {
 
-// 	},
-// });
+        frm.set_query("link_colaborador", function () {
+            return {
+                filters: {
+                    activo_sistema: 1,
+                },
+            };
+        });
+    },
+
+    refresh: function(frm) {
+        frm.fields_dict["tbl_detalleasignacion"].grid.get_field("equipo").get_query = function(doc, cdt, cdn) {
+            return {
+                filters: {
+                    activo_sistema: 1
+                }
+            };
+        };
+    }
+});
